7d7a6a710034562f477ab852df4e399799afdf50
Jens Reimann <jreimann@redhat.com>
2021-08-20 18:14:26 +0200

    initial import

---
+++ src/event.rs
@@ -0,0 +1,80 @@
+use serde::de::{EnumAccess, Error, MapAccess, SeqAccess};
+use serde::{de, Deserializer};
+use serde::{Deserialize, Serialize};
+use std::{fmt, time::Duration};
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+pub enum Record {
+    Suite(suite::Event),
+    Test(test::Event),
+}
+
+fn from_duration<'de, D>(d: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct DurationVisitor;
+
+    impl<'de> de::Visitor<'de> for DurationVisitor {
+        type Value = Duration;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a string containing json data")
+        }
+
+        fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
+        where
+            E: Error,
+        {
+            Ok(Duration::from_secs_f64(v))
+        }
+    }
+
+    d.deserialize_f64(DurationVisitor)
+}
+
+pub mod suite {
+    use super::*;
+
+    #[derive(Clone, Debug, Deserialize)]
+    #[serde(tag = "event", rename_all = "lowercase")]
+    pub enum Event {
+        Started {
+            test_count: u64,
+        },
+        Failed {
+            passed: u64,
+            failed: u64,
+            allowed_fail: u64,
+            ignored: u64,
+            filtered_out: u64,
+            #[serde(deserialize_with = "from_duration")]
+            exec_time: Duration,
+        },
+    }
+}
+
+pub mod test {
+    use super::*;
+
+    #[derive(Clone, Debug, Deserialize)]
+    #[serde(tag = "event", rename_all = "lowercase")]
+    pub enum Event {
+        Started {
+            name: String,
+        },
+        Ok {
+            name: String,
+            #[serde(deserialize_with = "from_duration")]
+            exec_time: Duration,
+        },
+        Failed {
+            name: String,
+            #[serde(deserialize_with = "from_duration")]
+            exec_time: Duration,
+            #[serde(default)]
+            stdout: String,
+        },
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+mod event;
+
+use crate::event::{suite, test, Record};
+use log::LevelFilter;
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+
+struct Processor<W>
+where
+    W: Write,
+{
+    write: W,
+}
+
+impl<W> Processor<W>
+where
+    W: Write,
+{
+    pub fn new(write: W) -> Self {
+        Self { write }
+    }
+
+    pub fn line(&mut self, line: &str) -> anyhow::Result<()> {
+        match serde_json::from_str(line) {
+            Ok(record) => self.record(record)?,
+            Err(err) => log::debug!("Ignoring line: {:?} -> {}", err, line),
+        }
+
+        Ok(())
+    }
+
+    fn record(&mut self, record: Record) -> anyhow::Result<()> {
+        log::debug!("Record: {:?}", record);
+
+        match record {
+            Record::Test(test::Event::Ok { name, exec_time }) => {
+                writeln!(self.write, "## ✅ {}", name)?;
+            }
+
+            Record::Test(test::Event::Failed {
+                name,
+                exec_time,
+                stdout,
+            }) => {
+                writeln!(self.write, "## ❌ {}", name)?;
+                if !stdout.is_empty() {
+                    writeln!(self.write)?;
+                    writeln!(self.write, "<details>")?;
+                    writeln!(self.write)?;
+
+                    writeln!(self.write, "<summary>Test output</summary>")?;
+                    writeln!(self.write)?;
+
+                    writeln!(self.write, "<pre>")?;
+                    writeln!(self.write, "{}", stdout)?;
+                    writeln!(self.write, "</pre>")?;
+
+                    writeln!(self.write)?;
+                    writeln!(self.write, "</details>")?;
+                    writeln!(self.write)?;
+                }
+            }
+
+            Record::Suite(suite::Event::Failed { passed, failed, .. }) => {
+                writeln!(self.write, "| Passed | Failed |")?;
+                writeln!(self.write, "| --- | --- |")?;
+                writeln!(self.write, "| {} | {} |", passed, failed)?;
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    TermLogger::init(
+        LevelFilter::Debug,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    let input = File::open("test-output.json")?;
+    let reader = BufReader::new(input);
+
+    let output = File::create("test-output.md")?;
+    let writer = BufWriter::new(output);
+
+    let mut processor = Processor::new(writer);
+
+    for line in reader.lines() {
+        processor.line(&line?);
+    }
+
+    Ok(())
+}
